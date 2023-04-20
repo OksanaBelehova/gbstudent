9692f1f2cb7e92f0964897d90a7d0270bd62db2b
OksanaBelehova <relativamente@mail.ru>
2023-04-20 21:41:15 +0300

    HW10

--- HW10.py
+++ HW10.py
@@ -54,4 +54,15 @@ class Dragon:
     def __repr__(self):
         return f'{self.height}, {self.danger}, {self.color}'
 
+dr = Dragon(69, 5, "brown")
+dr1 = Dragon(69, 5, "gray")
+print (dr > dr1, dr != dr1, dr <= dr1)
+print(dr, dr1, sep="\n")
+
+dr.change_color("white")
+dr -= 23
+dr1 -= 2
+dr2 = dr+dr1
+print(dr, dr1, dr2, sep="\n")
+
 # print(dr("Welcome")
